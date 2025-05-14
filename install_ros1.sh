50f5015b81b5bf32a53ecd2947a112dbe3f4718c
atom <145321614+vishnu1002@users.noreply.github.com>
2025-05-14 12:24:51 +0530

    Update install_ros1.sh

--- install_ros1.sh
+++ install_ros1.sh
@@ -6,9 +6,7 @@ sudo apt update && sudo apt upgrade -y
 # Add ROS repository
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-# Add ROS key
-sudo apt update
-sudo apt install -y curl
+sudo apt install curl -y
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 sudo apt update
